bcaaf52f86ba875c95851b36fa916ec9610465f5
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-08-06 07:35:57 +0530

    ⚠️

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from data import THE_ALTS
 API_ID = int(getenv("API_ID", "25981592"))
 API_HASH = getenv("API_HASH", "709f3c9d34d83873d3c7e76cdd75b866")
 SESSION1 = getenv("SESSION")
-ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/88f2548026fc038870f7b.jpg")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/056154992584a0faa0ec1.jpg")
 OWNER_ID = int(getenv("OWNER_ID", "6039423699"))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
